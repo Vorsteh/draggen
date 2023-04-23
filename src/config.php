f954cd5c1173c45101f1d821501656b46a013f63
Emil <Whyzoid@gmail.com>
2023-04-23 21:28:46 +0200

    idunnoprofile
    some profile stuff i guess i ont really know

--- src/config.php
+++ src/config.php
@@ -1,3 +1,7 @@
 <?php
 session_start();
 $conn = mysqli_connect('localhost', 'root', '', 'draggen') or die;
+
+if(!$conn){
+    echo "Database connected" . mysqli_connect_error();
+}
--- src/profile.php
+++ src/profile.php
@@ -1,4 +1,5 @@
 <?php
+require 'config.php';
 if(!empty($_SESSION['id'])){
     $id = $_SESSION['id'];
     $result = mysqli_query($conn, "SELECT * FROM users WHERE id = '$id'");
@@ -6,8 +7,8 @@ if(!empty($_SESSION['id'])){
 }else{
     header('Location: login.php');
 }
-
 ?>
+
 
 <!DOCTYPE html>
 <html lang="en">
@@ -15,9 +16,19 @@ if(!empty($_SESSION['id'])){
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../dist/output.css">
     <title>Profil</title>
 </head>
 <body>
-    
+    <?php 
+        $image = $row["profilepicture"];
+        $mail = $row['email'];
+        $usrname = $row['username'];
+    ?>
+    <div>
+        <img class='rounded-full' width='50px' src="../images/<?php echo $image?>" alt="">
+        <p>Användarnamn: <?php echo $usrname ?></p>
+        <p>Email: <?php echo $mail ?></p>
+    </div>
 </body>
 </html>
