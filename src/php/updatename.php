16bdba5bd7f9e4648ef998f8a772cef194aae238
Emil <Whyzoid@gmail.com>
2023-04-30 13:38:36 +0200

    a
    a

--- src/php/updatename.php
+++ src/php/updatename.php
@@ -1,6 +1,15 @@
 <?php
 require 'config.php';
+if(!empty($_SESSION['id'])){
+    $id = $_SESSION['id'];
+    $result = mysqli_query($conn, "SELECT * FROM users WHERE id = '$id'");
+    $row = mysqli_fetch_assoc($result);
 
-
+    $query = "UPDATE users SET VALUES('', '$name', '$username', '$email', '$passwordhash', 'noprofile.png')";
+    mysqli_query($conn, $query);
+}else{
+    header('Location: login.php');
+}
+?>
 
 ?>
